09624e327ca63391d40c7b9208a4a3600315ff00
Tamas Gal <tgal@km3net.de>
2018-03-26 16:49:26 +0200

    Load scripts in header

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,22 @@ from os.path import join
 from flask import render_template, send_from_directory
 from app import app
 
-app.config['PLOTS_PATH'] = "plots"
+
+PLOTS_PATH = "plots"
+
+
+PLOTS = [
+        ['dom_activity', 'dom_rates'],
+        ['pmt_rates', 'pmt_hrv'],
+        ['trigger_rates'],
+        ]
+
 
 @app.route('/')
 def index():
-    return render_template('plots.html')
+    return render_template('plots.html', plots=PLOTS)
+
 
 @app.route('/plots/<path:filename>')
 def custom_static(filename):
-    return send_from_directory(join(app.root_path, "../plots"), filename)
+    return send_from_directory(join(app.root_path, PLOTS_PATH), filename)
