b03ab4673cddea0b7a1dc4edaeb38657ca88cbd2
Tamas Gal <tgal@km3net.de>
2019-05-19 22:58:30 +0200

    Restructure k40

--- scripts/k40_calibration.py
+++ scripts/k40_calibration.py
@@ -7,13 +7,13 @@ K40 Intra-DOM Calibration
 =========================
 
 Usage:
-    k40_calibration.py [options]
+    k40_calibration.py [options] -d DET_ID
     k40_calibration.py (-h | --help)
 
 Options:
+    -d DET_ID       Detector ID.
     -l LIGIER_IP    The IP of the ligier [default: 127.0.0.1].
     -p LIGIER_PORT  The port of the ligier [default: 5553].
-    -d DET_ID       Detector ID.
     -o PLOT_DIR     The directory to save the plot [default: plots].
     -h --help       Show this screen.
 
@@ -42,30 +42,28 @@ def main():
     det_oid = kp.db.DBManager().get_det_oid(det_id)
 
     pipe = kp.Pipeline(timeit=True)
-    pipe.attach(
-        kp.io.ch.CHPump,
-        host=ligier_ip,
-        port=ligier_port,
-        tags='IO_TSL1, IO_MONIT',
-        timeout=7 * 60 * 60 * 24,
-        max_queue=200000)
+    pipe.attach(kp.io.ch.CHPump,
+                name="K40_CHPump",
+                host=ligier_ip,
+                port=ligier_port,
+                tags='IO_TSL1, IO_MONIT',
+                timeout=7 * 60 * 60 * 24,
+                max_queue=200000)
     pipe.attach(kp.io.ch.CHTagger)
     pipe.attach(StatusBar, every=50000)
     pipe.attach(MemoryObserver, every=100000)
     pipe.attach(k40.MedianPMTRatesService, only_if='IO_MONIT')
     pipe.attach(kp.io.daq.TimesliceParser)
-    pipe.attach(
-        k40.TwofoldCounter,
-        tmax=10,
-        dump_filename=os.path.join(plots_path, 'twofold_counts.p'))
+    pipe.attach(k40.TwofoldCounter,
+                tmax=10,
+                dump_filename=os.path.join(plots_path, 'twofold_counts.p'))
     pipe.attach(Siphon, volume=10 * 60 * 180, flush=True)
     pipe.attach(k40.K40BackgroundSubtractor)
     pipe.attach(k40.IntraDOMCalibrator, ctmin=-1, det_id=det_id)
-    pipe.attach(
-        IntraDOMCalibrationPlotter,
-        det_oid=det_oid,
-        data_path=plots_path,
-        plots_path=plots_path)
+    pipe.attach(IntraDOMCalibrationPlotter,
+                det_oid=det_oid,
+                data_path=plots_path,
+                plots_path=plots_path)
     pipe.attach(k40.ResetTwofoldCounts)
     pipe.drain()
 
