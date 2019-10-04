b0241799200641ae571f8692747480483d425087
Tamas Gal <tgal@km3net.de>
2019-10-04 11:51:14 +0200

    Show day and month in xlabel

--- scripts/ahrs_calibration.py
+++ scripts/ahrs_calibration.py
@@ -108,8 +108,7 @@ class CalibrateAHRS(kp.Module):
 
     def create_plot(self):
         print(self.__class__.__name__ + ": updating plot.")
-        # xfmt = md.DateFormatter('%Y-%m-%d %H:%M')
-        xfmt = md.DateFormatter('%H:%M')
+        xfmt = md.DateFormatter('%Y-%m-%d %H:%M')
         xlim = (datetime.utcfromtimestamp(time.time() -
                                           self.time_range * 60 * 60),
                 datetime.utcnow())
