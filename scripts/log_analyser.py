f1686ccbec3e56e0eccff7dd6d29cd6678f499de
Tamas Gal <tgal@km3net.de>
2020-05-08 19:00:14 +0200

    Fix matplotlib

--- scripts/log_analyser.py
+++ scripts/log_analyser.py
@@ -6,6 +6,9 @@
 import sys
 import re
 import numpy as np
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os
 import datetime
